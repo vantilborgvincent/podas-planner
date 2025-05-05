5042fef30a8fb2f2d1cf9ef877a0654fed65d574
vantilborgvincent <vantilborgvincent1@gmail.com>
2025-05-05 16:37:14 +0200

    Create label.tsx

---
+++ components/ui/label.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+const Label = React.forwardRef
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={`text-sm font-medium leading-none ${className}`}
+      {...props}
+    />
+  )
+})
+Label.displayName = "Label"
+
+export { Label }
