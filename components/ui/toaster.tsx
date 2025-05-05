b5d72381445318de99686cd2d60991e1aad0c1a6
vantilborgvincent <vantilborgvincent1@gmail.com>
2025-05-05 16:43:33 +0200

    Create toaster.tsx

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export function Toaster() {
+  return (
+    <div className="fixed top-0 right-0 z-50 p-4">
+      {/* Toast notifications would appear here */}
+    </div>
+  )
+}
